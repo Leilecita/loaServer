a3c2c9e95a8725e4af650e5295c162840402087c
leila <delcampo.leila@gmail.com>
2019-03-24 15:57:57 -0300

    ultimos cambios

--- controllers/BoxesController.php
+++ controllers/BoxesController.php
@@ -9,12 +9,41 @@
 
 require_once 'BaseController.php';
 require_once  __DIR__.'/../models/BoxModel.php';
+require_once  __DIR__.'/../models/ExtractionModel.php';
 
 class BoxesController extends BaseController
 {
+    private $extractions;
+
     function __construct(){
         parent::__construct();
         $this->model = new BoxModel();
+        $this->extractions = new ExtractionModel();
     }
+
+    function getBoxes(){
+
+        $boxes=$this->getModel()->findAll($this->getFilters(),$this->getPaginator());
+        for ($i = 0; $i < count($boxes); ++$i) {
+
+            $created=$boxes[$i]['created'];
+
+            $parts = explode(" ", $created);
+            $date=$parts[0]." 00:00:00";
+
+            $next_date = date('Y-m-d', strtotime( $parts[0].' +1 day'));
+
+
+            $dateTo=$next_date." 00:00:00";
+
+            $totalAmount=$this->extractions->amountByExtractionsDay($date,$dateTo);
+
+            $this->model->update($boxes[$i]['id'],array('deposit' => $totalAmount));
+
+        }
+
+        $this->returnSuccess(200,$boxes);
+
+        }
 
 }
