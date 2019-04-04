870fe91e4b1c1741785abeb9c92fbef2b06412f5
leila <delcampo.leila@gmail.com>
2019-04-03 22:35:43 -0300

    se agrega foto a la caja

--- models/BoxModel.php
+++ models/BoxModel.php
@@ -16,5 +16,35 @@ class BoxModel extends BaseModel
         parent::__construct();
         $this->tableName = 'item_box';
     }
+
+    function save($data){
+        if(empty($data['imageData'])) {
+            unset($data['imageData']);
+            return $this->getDb()->insert($this->tableName, $data);
+        }else{
+            $filePath = '/uploads/users/'.time().'.jpg';
+            $this->base64_to_jpeg($data['imageData'],__DIR__.'/..'.$filePath);
+            unset($data['imageData']);
+            $data['image_url'] = $filePath;
+
+            return $this->getDb()->insert($this->tableName, $data);
+        }
+    }
+    function update($id,$data){
+
+        error_log(print_r($data,true));
+        if(empty($data['imageData'])) {
+            unset($data['imageData']);
+
+            return $this->getDb()->update($this->tableName, $data,['id' => "$id"]);
+        }else{
+            $filePath = '/uploads/users/'.time().'.jpg';
+            $this->base64_to_jpeg($data['imageData'],__DIR__.'/..'.$filePath);
+            unset($data['imageData']);
+            $data['image_url'] = $filePath;
+
+            return $this->getDb()->update($this->tableName, $data,['id' => "$id"]);
+        }
+    }
 }
 
