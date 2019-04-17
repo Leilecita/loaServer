64c5f4a97eed672bc73fa7b773b125f6862a26db
leila <delcampo.leila@gmail.com>
2019-04-17 19:02:32 -0300

    volviendo

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -50,8 +50,7 @@ abstract class BaseModel
 
     function getSpinner($filters=array(),$type){
         $conditions = join(' AND ',$filters);
-       // $query = 'SELECT DISTINCT '.$type.' FROM '.$this->tableName .( empty($filters) ?  '' : ' WHERE '.$conditions ).' ORDER BY created DESC';
-        $query = 'SELECT DISTINCT type FROM '.$this->tableName .( empty($filters) ?  '' : ' WHERE '.$conditions ).' ORDER BY created DESC';
+        $query = 'SELECT DISTINCT '.$type.' FROM '.$this->tableName .( empty($filters) ?  '' : ' WHERE '.$conditions ).' ORDER BY created DESC';
 
         return $this->db->fetch_all($query);
 
