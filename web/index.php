7b1e31d7c783a7d916a22eded4411c707ec70b11
leila <delcampo.leila@gmail.com>
2019-10-24 20:06:09 -0300

    avanzando pagina loa

--- web/index.php
+++ web/index.php
@@ -7,51 +7,68 @@
     <meta charset="utf-8">
     <title>Formulario de Registro LOA</title>
     <link href="estilos.css" rel="stylesheet" type="text/css">
-
 </head>
-<div class="title">
-    <h2><em>LOA ESCUELA DE SURF</em></h2>
 
-</div>
+
 
 <body>
-<div class="group">
 
+<div class="group">
     <form  action="registro.php" method="POST" >
 
-        <label for="nombre">Nombre  </label>
-        <input type="text" name="nombre" class="form-input" required/>
+        <div class="alumno">
+            <p><em>Datos alumno</em></p>
+        </div>
 
-        <label for="apellido">Apellido </label>
-        <input type="text" name="apellido" class="form-input" required/>
+        <input type="text" name="nombre" class="form-input" required placeholder="Nombre"/>
 
-        <label for="edad">Edad </label>
-        <input type="text" name="edad" class="form-input" text/>
+        <input type="text" name="apellido" class="form-input" required placeholder="Apellido"/>
 
-        <label for="dni">DNI </label>
-        <input type="text" name="dni" class="form-input" required/>
+        <input type="text" name="nacimiento" class="form-input" text placeholder="Fecha de nacimiento"/>
 
-        <label for="telefono">Telefono </label>
-        <input type="tel" name="telefono" class="form-input" text/>
+        <input type="text" name="edad" class="form-input" text placeholder="Edad"/>
 
-        <label for="email">Email </label>
-        <input type="email" name="email" class="form-input" />
+        <input type="text" name="dni" class="form-input" required placeholder="DNI"/>
 
-        <label for="redsocial">Facebook / Instagram </label>
-        <input type="text" name="redsocial" class="form-input" />
+        <input type="text" name="direccion" class="form-input" required placeholder="Dirección"/>
+
+        <div class="alumno">
+            <p><em>Datos padres</em></p>
+        </div>
+
+        <input type="text" name="nombremama" class="form-input" required  placeholder="Nombre mamá"/>
+
+        <input type="tel" name="telefonomama" class="form-input" text placeholder="Teléfono"/>
+
+        <input type="text" name="nombrepapa" class="form-input" required placeholder="Nombre papá"/>
+
+        <input type="tel" name="telefonopapa" class="form-input" text placeholder="Teléfono"/>
+
+        <input type="email" name="email" class="form-input" placeholder="Email" />
+
+
+        <input type="text" name="redsocial" class="form-input" placeholder="Instagram" />
 
         <center> <input class="form-btn" name="submit" type="submit" value="Suscribirse" /></center>
         </p>
 
-
     </form>
-</div>
-<div class = "obs">
-    <p><em>La dirección de correo electrónico
-            que usted ha proporcionado ha sido previamente registrada en nuestra base de datos.</em></p>
 </div>
 
 
 
 </body>
 </html>
+
+<!--
+
+<div class="title">
+    <h2><em>LOA ESCUELA DE SURF</em></h2>
+</div>
+
+<div class = "obs">
+    <p><em>La dirección de correo electrónico
+            que usted ha proporcionado ha sido previamente registrada en nuestra base de datos.</em></p>
+</div>
+
+-->
