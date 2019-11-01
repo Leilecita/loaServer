98c967733ab31815f178f02553c68841e1c01d24
leila <delcampo.leila@gmail.com>
2019-11-01 16:10:01 -0300

    cambios pagina

--- web/index.php
+++ web/index.php
@@ -5,56 +5,114 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Formulario de Registro LOA</title>
-    <link href="estilos.css" rel="stylesheet" type="text/css">
+    <link href="css/estilos.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </head>
 
 
 
 <body>
+<div class="background" ></div>
+<div class="front container">
+<div class="row">
+    <div class="col-sm-8 offset-sm-2 col-xs-10 offset-xs-1">
+        <form  action="registro.php" method="POST" >
+            <fieldset class="withLogo">
+                <img src="img/logoloa.png" class="logoloa" />
 
-<div class="group">
-    <form  action="registro.php" method="POST" >
+                <legend><h4>Datos alumno</h4></legend>
+                <div class="form-group">
+                    <label for="nombre" class="col-form-label">Nombre</label>
+                    <input type="text" name="nombre" class="form-control" required placeholder=""/>
+                </div>
+                <div class="form-group">
+                    <label for="apellido">Apellido</label>
+                    <input type="text" name="apellido" class="form-control"  placeholder=""/>
+                </div>
+                <div class="form-group">
+                    <label for="fecha_nacimiento">Fecha de nacimiento</label>
+                    <input type="text" name="fecha_nacimiento" class="form-control" text placeholder=""/>
+                </div>
+                <div class="form-group">
+                    <label for="edad">Edad</label>
+                    <input type="text" name="edad" class="form-control" text placeholder=""/>
+                </div>
+                <div class="form-group">
+                    <label for="dni">DNI</label>
+                    <input type="text" name="dni" class="form-control" text required placeholder=""/>
+                </div>
+                <div class="form-group">
+                    <label for="direccion">Dirección</label>
+                    <input type="text" name="direccion" class="form-control" text placeholder=""/>
+                </div>
 
-        <div class="alumno">
-            <p><em>Datos alumno</em></p>
-        </div>
+                <div class="row">
+                    <div class="col-sm-6 col-xs-12">
+                        <legend>Datos Mamá</legend>
+                        <div class="form-group">
+                            <label for="nombre_mama" class="col-form-label">Nombre</label>
+                            <input type="text" name="nombre_mama" class="form-control"  placeholder=""/>
+                        </div>
+                        <div class="form-group">
+                            <label for="tel_mama">Telefono</label>
+                            <input type="text" name="tel_mama" class="form-control"  placeholder=""/>
+                        </div>
 
-        <input type="text" name="nombre" class="form-input" required placeholder="Nombre"/>
+                        <div class="form-group">
+                            <label for="email_mama" class="col-form-label">Email</label>
+                            <input type="email" name="email_mama" class="form-control"  placeholder=""/>
+                        </div>
 
-        <input type="text" name="apellido" class="form-input" required placeholder="Apellido"/>
+                        <div class="form-group">
+                            <label for="instagram_mama">Instagram</label>
+                            <input type="text" name="instagram_mama" class="form-control"  placeholder=""/>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <legend>Datos Papá</legend>
+                        <div class="form-group">
+                            <label for="nombre_papa" class="col-form-label">Nombre </label>
+                            <input type="text" name="nombre_papa" class="form-control"  placeholder=""/>
+                        </div>
+                        <div class="form-group">
+                            <label for="tel_papa">Telefono </label>
+                            <input type="text" name="tel_papa" class="form-control"  placeholder=""/>
+                        </div>
 
-        <input type="text" name="nacimiento" class="form-input" text placeholder="Fecha de nacimiento"/>
+                        <div class="form-group">
+                            <label for="email_papa" class="col-form-label">Email</label>
+                            <input type="email" name="email_papa" class="form-control"  placeholder=""/>
+                        </div>
 
-        <input type="text" name="edad" class="form-input" text placeholder="Edad"/>
-
-        <input type="text" name="dni" class="form-input" required placeholder="DNI"/>
-
-        <input type="text" name="direccion" class="form-input" required placeholder="Dirección"/>
-
-        <div class="alumno">
-            <p><em>Datos padres</em></p>
-        </div>
-
-        <input type="text" name="nombremama" class="form-input" required  placeholder="Nombre mamá"/>
-
-        <input type="tel" name="telefonomama" class="form-input" text placeholder="Teléfono"/>
-
-        <input type="text" name="nombrepapa" class="form-input" required placeholder="Nombre papá"/>
-
-        <input type="tel" name="telefonopapa" class="form-input" text placeholder="Teléfono"/>
-
-        <input type="email" name="email" class="form-input" placeholder="Email" />
+                        <div class="form-group">
+                            <label for="instagram_papa">Instagram</label>
+                            <input type="text" name="instagram_papa" class="form-control"  placeholder=""/>
+                        </div>
+                    </div>
+                </div>
 
 
-        <input type="text" name="redsocial" class="form-input" placeholder="Instagram" />
 
-        <center> <input class="form-btn" name="submit" type="submit" value="Suscribirse" /></center>
-        </p>
+                </fieldset>
 
-    </form>
+
+
+
+
+            </p>
+            <button type="submit" class="btn btn-primary">Suscribirse</button>
+        </form>
+    </div>
 </div>
-
+</div>
+</div>
 
 
 </body>
@@ -62,13 +120,21 @@
 
 <!--
 
-<div class="title">
-    <h2><em>LOA ESCUELA DE SURF</em></h2>
-</div>
-
-<div class = "obs">
-    <p><em>La dirección de correo electrónico
-            que usted ha proporcionado ha sido previamente registrada en nuestra base de datos.</em></p>
-</div>
+ <fieldset class="form-group">
+                    <legend>Checkboxes</legend>
+                    <div class="form-check">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" value="" checked="">
+                            Option one is this and that—be sure to include why it's great
+                        </label>
+                    </div>
+                    <div class="form-check disabled">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" value="" disabled="">
+                            Option two is disabled
+                        </label>
+                    </div>
+                </fieldset>
+            </fieldset>
 
 -->
--- web/registro.php
+++ web/registro.php
@@ -9,16 +9,24 @@ $db_password=null;
 $db_name='loa';
 $db_table_name="students";
 
-   $db_connection = mysqli_connect($db_host, $db_user, $db_password);
+$db_connection = mysqli_connect($db_host, $db_user, $db_password);
+mysqli_select_db($db_connection,$db_name );
+
 
 if (!$db_connection) {
     die('No se ha podido conectar a la base de datos');
 }
 $subs_name = utf8_decode($_POST['nombre']);
 $subs_last = utf8_decode($_POST['apellido']);
-$subs_email = utf8_decode($_POST['email']);
+//$subs_email = utf8_decode($_POST['email']);
+$subs_nacimiento = utf8_decode($_POST['fecha_nacimiento']);
+$subs_edad = utf8_decode($_POST['edad']);
+$subs_dni = utf8_decode($_POST['dni']);
 
-$resultado=mysqli_query($db_connection,"SELECT * FROM ".$db_table_name." WHERE Email = '".$subs_email."'" );
+
+$resultado=mysqli_query($db_connection,"SELECT * FROM ".$db_table_name." WHERE dni = '".$subs_dni."'" );
+
+var_dump($resultado);
 
 if (mysqli_num_rows($resultado)>0)
 {
@@ -26,9 +34,9 @@ if (mysqli_num_rows($resultado)>0)
 
 } else {
 
-    $insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`Nombre` , `Apellido` , `Email`) VALUES ("' . $subs_name . '", "' . $subs_last . '", "' . $subs_email . '")';
+    $insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`nombre` , `apellido` ,`dni` , `edad` ,`fecha_nacimiento`) VALUES ("' . $subs_name . '", "' . $subs_last  . '",
+     "' . $subs_dni . '", "' . $subs_edad . '",  "' . $subs_nacimiento . '")';
 
-    mysqli_select_db($db_connection,$db_name );
     $retry_value = mysqli_query( $db_connection,$insert_value);
 
     if (!$retry_value) {
